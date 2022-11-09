73e46e14bee9a5734c887c781f85385d7894d88c
hongnhung <hongnhung10101998@gmail.com>
2022-11-09 16:49:16 +0700

    Fix UI

--- src/components/withoutRedux/SingleCounter.js
+++ src/components/withoutRedux/SingleCounter.js
@@ -60,25 +60,69 @@ class SingleCounter extends Component {
 
     render() {
         return (
-            <div>
-                <p className='content'>Result: {this.state.count}</p>
-                <br/>
-                <input type='text' pattern='[0-9]*' size='15' onChange={this.handleChangeQuantity} value={this.state.quantity} name='quantityInput'/>
-                <br/>
-                <button onClick={this.handleIncrease} className=''>+</button>
-                <button onClick={this.handleDecrease} className=''>-</button>
-                <br/>
-                <button onClick={this.state.isActivate? this.stopTimer.bind(this):
-                    this.startTimer.bind(this)} className=''>
-                        {this.state.isActivate ? `Stop` : `Start`}
-                </button>
-                <button 
+          // <div>
+          //     <p className='content'>Result: {this.state.count}</p>
+          //     <br/>
+          //     <input type='text' pattern='[0-9]*' size='15' onChange={this.handleChangeQuantity} value={this.state.quantity} name='quantityInput'/>
+          //     <br/>
+          //     <button onClick={this.handleIncrease} className=''>+</button>
+          //     <button onClick={this.handleDecrease} className=''>-</button>
+          //     <br/>
+          //     <button onClick={this.state.isActivate? this.stopTimer.bind(this):
+          //         this.startTimer.bind(this)} className=''>
+          //             {this.state.isActivate ? `Stop` : `Start`}
+          //     </button>
+          //     <button
+          //     style={{display: this.props.showDelete? '' : 'none'}}
+          //     onClick={()=>this.props.deleteCounter(this.props.id)}>
+          //     Delete
+          //     </button>
+          //     <br/>
+          // </div>
+          <div
+            id={"content-wrapper"}
+            className={"flex column vertical-align horizontal-align"} >
+            <h1>Simple Counter in React using Hooks</h1>
+            <div
+              id={"counter-wrapper"}
+              className={"flex column vertical-align justify-sp-ev"}
+            >
+              <div className={"flex column vertical-align"}>
+                <p>{this.state.count}</p>
+              </div>
+              <div>
+                <h3>Add / substract custom quantity</h3>
+                <div className={"flex row vertical-align horizontal-align"}>
+                  <input
+                    type="text"
+                    pattern='[0-9]*' 
+                    value={this.state.quantity}
+                    onChange={this.handleChangeQuantity}
+                    className={"mr-10"}
+                  />
+
+                  <button
+                    className={"mr-5 width-40"}
+                    onClick={this.handleIncrease}
+                  >
+                    -
+                  </button>
+                  <button className={"width-40"} onClick={this.handleIncrease}>
+                    +
+                  </button>
+                </div>
+              </div>
+              <button onClick={this.state.isActivate? this.stopTimer.bind(this):
+                  this.startTimer.bind(this)} className=''>
+                      {this.state.isActivate ? `Stop` : `Start`}
+              </button>
+              <button
                 style={{display: this.props.showDelete? '' : 'none'}}
                 onClick={()=>this.props.deleteCounter(this.props.id)}>
                 Delete
-                </button>
-                <br/>
+              </button>
             </div>
+          </div>
         );
     }
 }
